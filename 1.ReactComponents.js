8cc4cd2591575a0cad78e2b23b47b72580c2f42f
Tusar Haque <tusarhaque12@gmail.com>
2019-04-23 01:43:20 +0600

    React Componets

--- 1.ReactComponents.js
+++ 1.ReactComponents.js
@@ -1,33 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 const owl = {
-  title: 'Excellent Owl',
-  src: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg'
+  title: "Excellent Owl",
+  src:
+    "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg"
 };
 // Component class starts here:
 class Owl extends React.Component {
-  render(){
-  return (
-    <div>
-      <h1>{owl.title}</h1>
-      <img
-        src = {owl.src}
-        alt = {owl.title}
-        
-        />
-    
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <h1>{owl.title}</h1>
+        <img src={owl.src} alt={owl.title} />
+      </div>
+    );
   }
-  
 }
 
-ReactDOM.render(<Owl />,
-  document.getElementById('app')
-)
-
-
-
+ReactDOM.render(<Owl />, document.getElementById("app"));
